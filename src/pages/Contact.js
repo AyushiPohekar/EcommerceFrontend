5d120433184713a04c354b674148f233b82c4a7e
Ayushi Rhutwik Pohekar <ayushi2021.29@gmail.com>
2023-04-03 11:13:40 +0100

    ContactUs done

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,10 +1,28 @@
 import React from 'react'
-import Layout from '../components/Layout/Layout'
-
+import Layout from '../components/Layout/Layout';
+import {BiMailSend, BiPhoneCall, BiSupport} from "react-icons/bi";
+import './Pages.css'
 const Contact = () => {
   return (
     <Layout>
-    <h1>Contact</h1>
+    <div class="container ContactUs ">
+    <div class="row ContactUsrow">
+      <div class=" col-md-6">
+    <img src='https://creativmomentum.com/wp-content/uploads/2020/09/qualis-web-design-about-us.gif' alt='ContactUs'/>
+      </div>
+      <div class="col-md-6">
+   <div className='ContactUsRight'>
+   <h1 class='ContactUsTitle'>Contact Us</h1>
+   <p>For query about the product,Feel free to call!</p>
+   <p><BiMailSend/> : www.yourshop@ecommerce.com</p>
+   <p><BiPhoneCall/> : 0123-123456789</p>
+   <p><BiSupport/> : 1800-666-444(Toll Free)</p>
+   
+   </div>
+      </div>
+     
+    </div>
+  </div>
     </Layout>
   )
 }
--- src/pages/PageNotFound.js
+++ src/pages/PageNotFound.js
@@ -1,10 +1,19 @@
 import React from 'react'
 import Layout from '../components/Layout/Layout'
+import './Pages.css'
+import { useNavigate } from 'react-router-dom'
 
 const PageNotFound = () => {
+  const navigate=useNavigate();
   return (
     <Layout>
-    <h1>Page not found</h1>
+    <div className='pageNotFoundCpontainer'>
+    <h1 >404</h1>
+    <p >It seems that you are lost in perpetual black hole. let us help guide you out and get back you to home page</p>
+    <button onClick={()=>navigate('/')}>Home</button>
+    
+    </div>
+   
     </Layout>
   )
 }
